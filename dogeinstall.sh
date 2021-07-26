e4056532da35748966f10f51cd198c2f6cf730a0
ice147 <84690791+ice147@users.noreply.github.com>
2021-07-27 01:55:39 +0800

    Add files via upload

---
+++ dogeinstall.sh
@@ -0,0 +1,68 @@
+clear
+pkg install php -y
+pkg install git -y
+pkg install wget -y
+clear
+pkg install && pkg update
+pkg upgrade
+clear
+pkg install python
+pkg install python2
+pkg install zlib
+pkg install pip
+python pip install --upgrade pip
+python2 pip install --upgrade pip
+python3 pip install --upgrade pip
+clear
+pip install wheel
+pip install asyncio
+pip install telethon
+pip install colorama
+pip install bs4
+pip install requests
+pip install async_generator
+pip install requests
+pip install BeautifulSoup
+pip install bs4
+pip install colorama
+pip install datetime
+pip install marshel
+clear
+pip3 install -U collections -y
+pip3 install -U collections.abc -y
+pip3 install -U concurrent.futures -y
+pip3 install -U functools -y
+pip3 install -U heapq -y
+pip3 install -U itertools -y
+pip3 install -U os -y
+pip3 install -U socket -y
+pip3 install -U stat -y
+pip3 install -U subprocess -y
+pip3 install -U threading -y
+pip3 install -U time -y
+pip3 install -U traceback -y
+pip3 install -U sys -y
+pip3 install -U warnings -y
+pip3 install -U weakref -y
+pip3 install -U constants -y
+pip3 install -U coroutines -y
+pip3 install -U events -y
+pip3 install -U exceptions -y
+pip3 install -U futures -y
+pip3 install -U protocols -y
+pip3 install -U sslproto -y
+pip3 install -U staggered -y
+pip3 install -U tasks -y
+pip3 install -U transports -y
+pip3 install -U trsock -y
+pip3 install -U logger -y
+pip3 install -U rsa  -y
+pip3 install -U pyaes -y
+pip3 install -U requests -y
+pip3 install -U asyncio -y
+pip3 install -U async_generator -y
+pip3 install -U colorama -y
+pip3 install -U bs4 -y
+clear
+cd
+mkdir doge && cd doge && wget https://github.com/ice147/dogeInstall_K1/blob/main/uptocoin.zip && clear && unzip uptocoin && php bot.php
